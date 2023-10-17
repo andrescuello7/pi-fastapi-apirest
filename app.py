9f07aab801277f01568931ee6b3d031548dc8af6
andrescuello7 <andrescuellotrabajo@gmail.com>
2023-10-16 23:59:32 -0300

    add correction for deploy with render.com

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from config.database import engine
 from src.routes.character_router import router
 from src.models.character_models import Base
+import uvicorn
 
 #Server running from FastAPI
 app = FastAPI()
@@ -11,3 +12,6 @@ app.include_router(router)
 
 # Create Models in Database
 Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    uvicorn.run("__main__:app", host="0.0.0.0", port=8080, reload=True, workers=2)
